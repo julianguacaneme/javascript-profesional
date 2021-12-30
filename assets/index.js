f4e556df70e5ed018dc946c96bcb7544b9eaa0e1
julianguacaneme <julianguacaneme@gmail.com>
2021-12-30 09:40:07 -0500

    2

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,9 @@
-import MediaPlayer from './mediaPlayer';
+import MediaPlayer from './mediaPlayer.js';
+import AutoPlay from '../plugins/AutoPlay.js'
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [
+    new AutoPlay()] });
 
 const button = document.querySelector("button");
 button.onclick = () => player.togglePlay();
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,10 +1,21 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+};
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
 };
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
 };
+
+
 
 MediaPlayer.prototype.pause = function () {
   this.media.pause();
@@ -17,6 +28,15 @@ MediaPlayer.prototype.togglePlay = function () {
     this.pause();
   }
 };
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.unmuted = false;
+};
+
 
 export default MediaPlayer;
 
